1a6ab8447af744fa9cf5a6a91cf15b976dcc07f9
Aayush Sinha <aayush.aryan@me.com>
2022-09-15 10:15:08 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ const Nav = () => {
   return (
     <nav>
       <h2>Emojee</h2>
-      <a href="https://github.com/aayusharyan/emojee-react" target="_blank">Github</a>
+      <a href="https://github.com/aayusharyan/emojee-react" target="_blank" rel="noreferrer">Github</a>
     </nav>
   )
 }
